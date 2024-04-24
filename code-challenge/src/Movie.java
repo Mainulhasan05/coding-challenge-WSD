187528283917e6cb461ad758ebb9e40830b1af06
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-04-24 13:12:08 +0600

    initial project

---
+++ code-challenge/src/Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    private String movieName;
+    private int year;
+    public Movie(String movieName, int year) {
+        this.movieName = movieName;
+        this.year = year;
+    }
+}
